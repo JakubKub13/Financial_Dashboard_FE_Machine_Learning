1a5d9a10bb1c6c4b2dee91967b89ba331e3fd4f8
JakubKub13 <jakubkubala3@gmail.com>
2023-07-30 18:50:31 +0200

    Adding api

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,8 +1,10 @@
-import { Box, useTheme } from '@mui/material'
+import { Box, useMediaQuery } from '@mui/material'
+import Row1 from './Row1';
+import Row2 from './Row2';
+import Row3 from './Row3';
 
-type Props = {}
 
-const gridTemplate = `
+const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
@@ -13,19 +15,63 @@ const gridTemplate = `
     "g h i"
     "g h j"
     "g h j"
-`
+`;
 
-const Dashboard = (props: Props) => {
-    const { palette } = useTheme();
+const gridTemplateSmallScreens = `
+    "a"
+    "a"
+    "a"
+    "a"
+    "b"
+    "b"
+    "b"
+    "c"
+    "c"
+    "c"
+    "d"
+    "d"
+    "d"
+    "e"
+    "e"
+    "f"
+    "f"
+    "f"
+    "g"
+    "g"
+    "g"
+    "h"
+    "h"
+    "h"
+    "h"
+    "i"
+    "i"
+    "j"
+    "j"
+`;
 
 
+
+const Dashboard = () => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
 
    return (
      <Box width="100%" height="100%" display="grid"  gap="1.5rem"
-        sx={{
-            gridTemplateAreas: gridTemplate,
-        }}
-     ></Box>
+        sx={
+            isAboveMediumScreens ? {
+                gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+                gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+                gridTemplateAreas: gridTemplateLargeScreens,
+        } : {
+            gridAutoColumns: "1fr",
+            gridAutoRows: "80px",
+            gridTemplateAreas: gridTemplateSmallScreens,
+        }
+    }
+     >
+        <Row1 />
+        <Row2 />
+        <Row3 />
+     </Box>
    )
 }
 
